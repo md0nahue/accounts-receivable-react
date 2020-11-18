07c4a450d33ea04c995e04977a9a1d5c59a645f2
Tommy Groshong <tom@groshong.software>
2020-11-17 23:39:17 -0500

    Reorder app imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import Header from "./Header";
 import { Invoice } from "./Invoice";
 import { InvoicesForm } from "./InvoicesForm";
 import { InvoiceList } from "./InvoiceList";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
