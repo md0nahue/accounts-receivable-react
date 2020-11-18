93a2a054667dde9c99342c95435a2ce903e35ecd
Tommy Groshong <tom@groshong.software>
2020-11-17 23:34:07 -0500

    Fix import bad import

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
-import Invoice from "./Invoice";
+import { Invoice } from "./Invoice";
 import InvoicesForm from "./InvoicesForm";
 import InvoiceList from "./InvoiceList";
 import { BrowserRouter as Router, Route } from "react-router-dom";
