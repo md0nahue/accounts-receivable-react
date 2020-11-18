b1738572993c1a2fe5b79a075ff533f28c979210
Tommy Groshong <tom@groshong.software>
2020-11-17 23:41:55 -0500

    Extract Redux store create to separate module

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 import "./index.css";
 import { App } from "./components/App";
 import * as serviceWorker from "./serviceWorker";
-import reducer from "./reducers";
-import { addInvoice } from "./actions";
 import reportWebVitals from "./reportWebVitals";
-
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+import { store } from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -25,104 +18,6 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-store.dispatch(
-  addInvoice({
-    id: "1",
-    client: "CircleCi",
-    attn: "Bo <bo@circleci.com>",
-    due_date: "10/10/2020",
-    notes: "Send monthly invoices.",
-    projects: [
-      {
-        name: "Anti-virus Software",
-        description: "Subscription service for virus software.",
-        discount_percent: 40,
-        price_cents: 20000,
-        quantity: 1
-      },
-      {
-        name: "AWS",
-        description: "Monthly server costs.",
-        discount_percent: null,
-        price_cents: 380000,
-        quantity: 1
-      }
-    ],
-    services: [
-      {
-        name: "Software Development",
-        description: "Refactor of addresses module.",
-        discount_percent: null,
-        price_cents: 15000,
-        quantity: 8
-      },
-      {
-        name: "Software Development",
-        description: "Extract micro-service.",
-        discount_percent: null,
-        price_cents: 15000,
-        quantity: 12
-      },
-      {
-        name: "Software Development",
-        description: "Deploy new staging environment.",
-        discount_percent: null,
-        price_cents: 15000,
-        quantity: 9
-      }
-    ]
-  })
-);
-
-store.dispatch(
-  addInvoice({
-    id: "2",
-    client: "New Relic",
-    attn: "Matan <Matan@new-relic.com>",
-    due_date: "10/20/2020",
-    notes: "Send weekly invoices.",
-    projects: [
-      {
-        name: "Video Software",
-        description: "Subscription service for video software.",
-        discount_percent: null,
-        price_cents: 14000,
-        quantity: 1
-      },
-      {
-        name: "Heroku",
-        description: "Monthly server costs.",
-        discount_percent: 50,
-        price_cents: 28000,
-        quantity: 1
-      }
-    ],
-    services: [
-      {
-        name: "Software Development",
-        description: "Refactor of node modules.",
-        discount_percent: null,
-        price_cents: 15000,
-        quantity: 8
-      },
-      {
-        name: "Software Development",
-        description: "Extract service layer.",
-        discount_percent: null,
-        price_cents: 15000,
-        quantity: 12
-      },
-      {
-        name: "Software Development",
-        description: "Deploy product environments.",
-        discount_percent: null,
-        price_cents: 15000,
-        quantity: 9
-      }
-    ]
-  })
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/store.js
@@ -0,0 +1,107 @@
+import { createStore } from "redux";
+import reducer from "./reducers";
+import { addInvoice } from "./actions";
+
+export const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// Sets up example data
+store.dispatch(
+  addInvoice({
+    id: "1",
+    client: "CircleCi",
+    attn: "Bo <bo@circleci.com>",
+    due_date: "10/10/2020",
+    notes: "Send monthly invoices.",
+    projects: [
+      {
+        name: "Anti-virus Software",
+        description: "Subscription service for virus software.",
+        discount_percent: 40,
+        price_cents: 20000,
+        quantity: 1
+      },
+      {
+        name: "AWS",
+        description: "Monthly server costs.",
+        discount_percent: null,
+        price_cents: 380000,
+        quantity: 1
+      }
+    ],
+    services: [
+      {
+        name: "Software Development",
+        description: "Refactor of addresses module.",
+        discount_percent: null,
+        price_cents: 15000,
+        quantity: 8
+      },
+      {
+        name: "Software Development",
+        description: "Extract micro-service.",
+        discount_percent: null,
+        price_cents: 15000,
+        quantity: 12
+      },
+      {
+        name: "Software Development",
+        description: "Deploy new staging environment.",
+        discount_percent: null,
+        price_cents: 15000,
+        quantity: 9
+      }
+    ]
+  })
+);
+
+store.dispatch(
+  addInvoice({
+    id: "2",
+    client: "New Relic",
+    attn: "Matan <Matan@new-relic.com>",
+    due_date: "10/20/2020",
+    notes: "Send weekly invoices.",
+    projects: [
+      {
+        name: "Video Software",
+        description: "Subscription service for video software.",
+        discount_percent: null,
+        price_cents: 14000,
+        quantity: 1
+      },
+      {
+        name: "Heroku",
+        description: "Monthly server costs.",
+        discount_percent: 50,
+        price_cents: 28000,
+        quantity: 1
+      }
+    ],
+    services: [
+      {
+        name: "Software Development",
+        description: "Refactor of node modules.",
+        discount_percent: null,
+        price_cents: 15000,
+        quantity: 8
+      },
+      {
+        name: "Software Development",
+        description: "Extract service layer.",
+        discount_percent: null,
+        price_cents: 15000,
+        quantity: 12
+      },
+      {
+        name: "Software Development",
+        description: "Deploy product environments.",
+        discount_percent: null,
+        price_cents: 15000,
+        quantity: 9
+      }
+    ]
+  })
+);
