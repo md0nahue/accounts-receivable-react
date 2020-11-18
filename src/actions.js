5ebf09c9e388433d8c4a488fdf1e5b1ae64fec68
Tommy Groshong <tom@groshong.software>
2020-11-18 00:23:49 -0500

    Automatically add ID when adding invoice

--- src/actions.js
+++ src/actions.js
@@ -2,6 +2,10 @@ export const types = {
   ADD_INVOICE: "ADD_INVOICE"
 };
 
+let nextId = 10;
 export function addInvoice(invoice) {
-  return { type: types.ADD_INVOICE, payload: invoice };
+  return {
+    type: types.ADD_INVOICE,
+    payload: { ...invoice, id: String(++nextId) }
+  };
 }
