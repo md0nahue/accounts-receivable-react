bce435900d67dac689497ae1723c7a2c15391df2
Tommy Groshong <tom@groshong.software>
2020-11-17 23:28:53 -0500

    Add optional perf system

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import reducer from "./reducers";
 import { addInvoice } from "./actions";
+import reportWebVitals from "./reportWebVitals";
 
 const store = createStore(
   reducer,
@@ -122,3 +123,8 @@ store.dispatch(
     ]
   })
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
