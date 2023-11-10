8fa4605d5e3e5e77a75709144f485d750da701ce
Jamie Phelps <jamie@jamiephelps.com>
2023-11-10 10:56:39 -0800

    Remove vestige of todos

--- src/reducers.js
+++ src/reducers.js
@@ -2,7 +2,7 @@ import { types } from "./actions";
 
 const initialState = { invoices: [], activeInvoiceId: -1 };
 
-export default function todos(state = initialState, action) {
+export default function invoices(state = initialState, action) {
   switch (action.type) {
     case types.ADD_INVOICE:
       return { ...state, invoices: [...state.invoices, action.payload] };
